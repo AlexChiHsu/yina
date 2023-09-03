9061d07316c50da9276c99be42fe92ff0c8927c1
AlexHsu <x821209@gmail.com>
2023-09-03 16:57:03 +0800

    設定

---
+++ tailwind.config.js
@@ -0,0 +1,93 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = {
+  purge: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{html,js,ts,tsx,jsx}",
+    "./src/**/**/**/*.{html,js,ts,tsx,jsx}",
+    "./src/*.{html,js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))",
+        14: "repeat(14, minmax(0, 1fr))",
+        15: "repeat(15, minmax(0, 1fr))",
+        16: "repeat(16, minmax(0, 1fr))",
+      },
+      animation: {
+        fade: "fadeOut 5s ease-in-out",
+      },
+      keyframes: () => ({
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+      }),
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))",
+        14: "repeat(14, minmax(0, 1fr))",
+        15: "repeat(15, minmax(0, 1fr))",
+        16: "repeat(16, minmax(0, 1fr))",
+      },
+      backgroundImage: {
+        slider: "linear-gradient(91.47deg, #C10171 3.73%, #5C00F2 100%)",
+      },
+      screens: {
+        sm: "320px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "720px",
+        // => @media (min-width: 720px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+  },
+  plugins: [],
+});
