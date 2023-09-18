997a8d277809f1b36a529016bb9af6bc864c0bab
AlexChiHsu <smilechi1993@gmail.com>
2023-09-18 16:00:12 +0800

    還原

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Outlet } from "react-router-dom";
-import { useEffect } from "react";
 
 const AppContainer = styled.div`
   ${tw`
@@ -11,29 +10,6 @@ const AppContainer = styled.div`
 `;
 
 function App() {
-  useEffect(() => {
-    (window as any).fbAsyncInit = function () {
-      FB.init({
-        // this FB errors out
-        appId: "6896601727027597",
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: "v18.0",
-      });
-    };
-
-    (function (d: any, s: any, id: any) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
   return (
     <AppContainer>
       <Outlet />
