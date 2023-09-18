1e32f40bf01deb73ea620de4893d196fbe0a9662
AlexChiHsu <smilechi1993@gmail.com>
2023-09-18 13:51:19 +0800

    fb video

--- src/screens/test/index.tsx
+++ src/screens/test/index.tsx
@@ -18,7 +18,7 @@ const TestContainer = styled.div`
 const IframeContainer = styled.div`
   ${tw`
     // h-fit
-    h-[100%]
+    h-full
     flex-1
     // w-full
     flex
@@ -34,46 +34,28 @@ export default function Test() {
     <TestContainer>
       <IframeContainer>
         <Iframe
+          src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F163671912285037%2F&show_text=true&t=0"
           url="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F163671912285037%2F&show_text=true&t=0"
           id=""
           className=""
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share;"
-          allowFullScreen
-          sandbox="allow-modals"
-          // styles={{
-          //   background: "white",
-          //   width: "100%",
-          //   height: "100%",
-          //   position: "absolute",
-          //   top: 0,
-          //   left: 0,
-          // }}
         />
       </IframeContainer>
       <IframeContainer>
         <Iframe
           url="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F1207148619771662%2F&show_text=true&width=560&t=0"
-          width="100%"
-          height="100%"
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F1207148619771662%2F&show_text=true&width=560&t=0"
           id=""
           className=""
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share;"
-          allowFullScreen
-          sandbox="allow-modals"
-          styles={{ background: "white" }}
+          // styles={{ background: "white" }}
         />
       </IframeContainer>
       <IframeContainer>
         <Iframe
           url="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F432938068276457%2F&show_text=true&width=476&t=0"
-          width="100%"
-          height="100%"
+          src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fyinaamy%2Fvideos%2F432938068276457%2F&show_text=true&width=476&t=0"
           id=""
           className=""
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share;"
-          allowFullScreen
-          sandbox="allow-modals"
-          styles={{ background: "white" }}
+          // styles={{ background: "white" }}
         />
       </IframeContainer>
     </TestContainer>
